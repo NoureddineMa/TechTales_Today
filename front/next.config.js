d5b893ce5aab1c90a43aecd47dbab9d5fb1686fd
Youssef_BHL <ybhl83@gmail.com>
2023-10-20 12:49:58 +0100

    🎨 next js routing config

--- front/next.config.js
+++ front/next.config.js
@@ -2,6 +2,14 @@
 const path = require("path");
 
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/:slug*",
+        destination: "/pages/:slug*",
+      },
+    ];
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
   },
