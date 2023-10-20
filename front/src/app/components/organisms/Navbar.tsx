8e59edf8c83fa01e4dee4a0b63019d6e34dec9bb
Youssef_BHL <ybhl83@gmail.com>
2023-10-20 12:16:59 +0100

    🔥 remade some components code
    - Home Component
    - Navbar Component

--- front/src/app/components/organisms/Navbar.tsx
+++ front/src/app/components/organisms/Navbar.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import NavList from "../molecules/NavList";
 import NavActions from "../molecules/NavActions";
-import "../../../styles/components/_navbar.scss";
-
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="nav-container">
+    <header>
+      <nav>
         <div className="nav-logo">Riven</div>
         <NavList />
         <NavActions />
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 }
 
