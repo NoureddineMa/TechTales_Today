3055439121f5fdd7fd2e9ebee748192dff13fb5c
NoureddineMa <noureddinemaher13@gmail.com>
2023-11-09 22:53:34 +0100

    feat:create layout + register , login components

---
+++ front/src/app/login/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from "react";
+import Layout from "../components/layout";
+
+const Page = () => {
+  return (
+    <Layout>
+      <p>HELLO FROM LOGIN PAGE </p>
+    </Layout>
+  );
+};
+
+export default Page;
