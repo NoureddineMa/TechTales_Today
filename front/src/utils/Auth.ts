f4b4d4973d22e169090c8cca3e4f0d7773ad37cb
NoureddineMa <noureddinemaher13@gmail.com>
2023-11-11 15:39:22 +0100

    change base url to port 7000 and change url login and register too

--- front/src/utils/Auth.ts
+++ front/src/utils/Auth.ts
@@ -1,11 +1,11 @@
 import { API } from "./baseUrl";
 
 export async function Login(data:{}){
-  const res = API.post('/login',data)
+  const res = API.post('/auth/login',data)
   return res
 }
 export async function Register(data:{}){
-    const res = API.post('/register',data)
+    const res = API.post('/auth/register',data)
     return res
 }
 
