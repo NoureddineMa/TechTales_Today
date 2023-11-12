32abe3b717a7928a2668671b64144994c88c9c56
Youssef_BHL <ybhl83@gmail.com>
2023-11-12 01:06:35 +0100

    ➕ cors and bcrypt

--- front/next.config.js
+++ front/next.config.js
@@ -10,6 +10,14 @@ const nextConfig = {
       },
     ];
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
   },
